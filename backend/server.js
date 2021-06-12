dbd176de526a4bddfccab47db2e3050cecf68819
ULPvP <50048718+ULPvP@users.noreply.github.com>
2021-06-12 23:20:23 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -40,14 +40,14 @@ if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile('/client/build/index.html')
+    res.sendFile('../client/build/index.html')
   )
 }
 
 else {
 
-  app.use(express.static('client/build'))
-  app.use(express.static(`backend/images`))
+  app.use(express.static('../client/build'))
+  app.use(express.static(`../backend/images`))
   
 
 }
