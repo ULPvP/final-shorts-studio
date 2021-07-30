20f45295d9683f9cefe4b2cabc341cbe7f24fe62
ULPvP <50048718+ULPvP@users.noreply.github.com>
2021-07-31 00:46:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,8 +42,7 @@ if (process.env.NODE_ENV !== 'dev') {
    
 //   app.use('/', express.static(path.join(__dirname,'/client/build', '/index.html'));
 //   });
-    app.use('/', express.static(path.join(__dirname,'/client/build')))};
-
+    app.use('/', express.static(path.join(__dirname,'/client/build')))}
 
 
 
