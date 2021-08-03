f3c1b780a1f16d686d4610bca799d90eaf17dd9b
ULPvP <50048718+ULPvP@users.noreply.github.com>
2021-08-04 03:49:23 +0800

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,6 +7,12 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>Copyright &copy; 矮仔工作室</Col>
+          <a href="https://wa.me/c/85294582889">
+          <Col className='text-center py-3'>WhatsApp: +852 94582889 _Click Here_</Col>
+          </a>
+          <a href='https://www.instagram.com/shorts_studio/'>
+          <Col className='text-center py-3'>IG: Shorts_studio  _Click Here_</Col>
+          </a>
         </Row>
       </Container>
     </footer>
