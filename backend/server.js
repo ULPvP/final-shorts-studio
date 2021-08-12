b57512dfc7c4191622708f4ae02ad988d84c32e0
ULPvP <50048718+ULPvP@users.noreply.github.com>
2021-08-13 04:26:38 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,12 +38,12 @@ const __dirname = path.resolve()
 app.use( '/uploads', express.static( path.join( __dirname, '/uploads' ) ) )
 
 if ( process.env.NODE_ENV !== 'dev' ) {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
-  app.use('/',express.static(path.join(__dirname, '/frontend/build')))
+  app.use('/',express.static(path.join(__dirname, '/client/build')))
 }
 app.use( notFound )
 app.use( errorHandler )
